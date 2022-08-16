6790a9f41e29b54970504ebc7f5a9f1a203234a4
Tyler Neal <will1neal27@gmail.com>
2022-08-16 11:24:13 -0400

    adding stuff from the main

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,21 @@
 // var yelpApiKey = 'k4I2EBLwowihaRkeoPtje_qa1xss9NYOJgm5OQLCRNKb66_JliGrhnWlUnnUV952cDYjANT97B8AxJCy1XxtuGiJIwc5xENkXP4n_Bz1OiTgnm63JbvRM62Dxg_7YnYx'
 
 function placesToEat() {
-    var url = 'https://api.yelp.com/v3/businesses/search?term=food&location=columbus,oh'
+    var url = 'http://russelldev-cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=food&location=columbus,oh'
 
-    fetch(url)
+    fetch(url, {
+        method: 'GET',
+        headers: {
+            'Authorization': 'Bearer k4I2EBLwowihaRkeoPtje_qa1xss9NYOJgm5OQLCRNKb66_JliGrhnWlUnnUV952cDYjANT97B8AxJCy1XxtuGiJIwc5xENkXP4n_Bz1OiTgnm63JbvRM62Dxg_7YnYx',
+            'Access-Control-Allow-Origin': '*'
+        }
+    })
     .then(function(response) {
         return response.json();
     })
     .then(function(data) {
         console.log(data)
+        
     })
 }
 placesToEat()
