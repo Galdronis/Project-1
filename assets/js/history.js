531edbe553391a4a44930abfdf914b539212c0cd
Nicholas148894 <nich.oettinger@gmail.com>
2022-08-17 13:28:48 -0400

    button progress

--- assets/js/history.js
+++ assets/js/history.js
@@ -1,44 +1,26 @@
 var btn = document.querySelector(".btn");
+var generateFact = document.getElementById("generate")
+var sports = document.getElementById("sports")
+var random = document.getElementById("random")
+var social = document.getElementById("social")
+var food = document.getElementById("food")
 
-btn.addEventListener("click", generateFact);
-
-
-
-function generateSportsArray() {
-
-    var sportsArray = [
+var sportsArray = [
         "The NFL headquartered in columbus in the 1930s!",
         "The OSU marching band is also known as The Best Damn Band in the Land!",
 ]
 
-var randomFact = Math.floor(Math.random() * sportsArray.length)
-document.getElementById("generate").textContent = sportsArray[randomFact]
-
-}
-
-function generateRandomArray() {
-    var randomArray = [
+var randomArray = [
         "The tallest building in Columbus is the Rhodes State Office Tower standing at nearly 650 feet!",
         "Columbus is the home city of many famous people including Simone Biles and R.L. Stine!",
 ]
 
-var randomFact = Math.floor(Math.random() * randomArray.length)
-document.getElementById("generate").textContent = randomArray[randomFact]
-
-}
-
-function generateFoodArray() {
-    var foodArray = [
+var foodArray = [
         "The very first Wendy's was opened here!",
         "Famous chef Guy Fieri was born here!",
     ]
 
-var randomFact = Math.floor(Math.random() * foodArray.length)
-document.getElementById("generate").textContent = foodArray[randomFact]
-}
-
-function generateSocialArray() {
-    var socialArray = [
+var socialArray = [
         "Columbus is the third most important city in the country for fashion!",
         "Ohio obtained statehood in 1803, but Columbus wasn't the capitol until 1816!",
         "forty eight percent of Americans live within a day's drive from Columbus!",
@@ -47,12 +29,8 @@ function generateSocialArray() {
         "Columbus is home to one of the biggest Pride events in the country!",
 ]
 
-var randomFact = Math.floor(Math.random() * socialArray.length)
-document.getElementById("generate").textContent = socialArray[randomFact]
-    
 
-}
-
+btn.addEventListener("click", generateFact);
 
 
 
