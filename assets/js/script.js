298b3c8dedbb03f3882dce6195be53d96bd6dcc3
Wolfgang <18wrightm@gmail.com>
2022-08-17 10:34:44 -0400

    trying to fix the fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,13 @@
 var restaurantDisplayEl = document.getElementById('restaurant-display')
 
 function placesToEat() {
-    var url = 'http://russelldev-cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=food&location=columbus,oh'
+    var url = 'https://russelldev-cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=food&location=columbus,oh'
 
     fetch(url, {
         method: 'GET',
         headers: {
             'Authorization': 'Bearer k4I2EBLwowihaRkeoPtje_qa1xss9NYOJgm5OQLCRNKb66_JliGrhnWlUnnUV952cDYjANT97B8AxJCy1XxtuGiJIwc5xENkXP4n_Bz1OiTgnm63JbvRM62Dxg_7YnYx',
-            'Access-Control-Allow-Origin': '*'
+            
         }
     })
     .then(function(response) {
