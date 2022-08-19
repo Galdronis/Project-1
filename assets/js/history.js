b1ca7a80006136b1cdb97a32eeff5deb1dbfdcb0
Nicholas148894 <nich.oettinger@gmail.com>
2022-08-19 11:19:52 -0400

    added more facts

--- assets/js/history.js
+++ assets/js/history.js
@@ -12,39 +12,36 @@ var sportsArray = [
         "The OSU marching band is also known as The Best Damn Band in the Land!",
         "The Old Columbus Crew Stadium was the first dedicated soccer stadium in the country!",
         "Actor and voice actor J.K. Simmons is a huge Ohio State Buckeyes fan!",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
+        "Archie Griffith is the only college football player to win two Heisman trophies!",
+        "The last professional championship a Columbus sports team won was the MLS cup in 2020!",
+        "The Arnold Strongman Classic is held in Columbus!",
+        "The largest single breed horse show in America is held in Columbus!",
+        "Columbus is the only city in the country with two soccer specific stadiums!",
+        "Former crew coach Gregg Berhalter is the current U.S. National Soccer Team coach",
+        "There are eleven former Ohio State Buckeye players in the NFL hall of fame!",
 ]
 
 var randomArray = [
         "The tallest building in Columbus is the Rhodes State Office Tower standing at nearly 650 feet!",
-        "Columbus is the home city of many famous people including Simone Biles and R.L. Stine!",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
+        "Nate is bald",
+        "Woody Harrelson was arrested in 1982 in Columbus for dancing in the street!",
+        "Initially, Columbus was planned to be named Ohio City!",
+        "The Ohio Historical Center has a genuine two headed calf on display!",
+        "in 1926, a cow was nominated as Ohio State's homecoming queen!",
+        "Indianola Junior High School was the first junior high school opened in the country!",
+        "The average age of Columbus residents is among the lowest in the entire country at 30-33 years old!",
+        "The world's first water filtration facility was built here!",
+        "Columbus ranks in the top 20 most educated cities in the country!",
 ]
 
 var foodArray = [
         "The very first Wendy's was opened here!",
         "Fifty thousand pounds of chocolate a day are produced at the Anthony Thomas Candy Company in Columbus",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        
+        "Man vs Food has been to Columbus five times!",
+        "White Castle is headquartered in Columbus!",
+        "Buckeyes were invented in Columbus!",
+        "Square cut pizza with thin crust is also known as Columbus style pizza!",
+        "Schmidt's Sausage Haus is a nationally known German sausage house!",
     ]
 
 var socialArray = [
@@ -54,10 +51,10 @@ var socialArray = [
         "Lincoln learned he would become president while at the columbus statehouse!",
         "Columbus is home to the country's first ever standalone pediatric mental health pavilion!",
         "Columbus is home to one of the biggest Pride events in the country!",
-        "",
-        "",
-        "",
-        "",
+        "Famous playboy Hugh Heffner had a Playboy Mansion built here!",
+        "Columbus holds an annual Doo Dah Parade to celebrate libert, lunacy and satire!",
+        "The Columbus Zoo and Aquarium had the oldest captive gorilla in history! Colo lived to be 60 years old.",
+        "There's a bronze statue of Arnold Schwarzenegger in downtown Columbus!",
 ]
 
 var famousArray = [
